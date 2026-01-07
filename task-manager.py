a4a08bf135c88b00fab4348812fa567766a21018
Matthew La <matthewla246@gmail.com>
2026-01-07 15:38:28 -0500

    feat: added in-progress and done parser

--- task-manager.py
+++ task-manager.py
@@ -24,6 +24,14 @@ upd_parser.add_argument("new", type=str, help="new name of task")
 del_parser = subparser.add_parser("delete", help="delete a task")
 del_parser.add_argument("num", type=int, help="id of task")
 
+#In-Progress Parser
+prog_parser = subparser.add_parser("mark-in-progress", help="sets status of task to in-progress")
+prog_parser.add_argument("val", type=int, help="id of task")
+
+#Done Parser
+done_parser = subparser.add_parser("mark-done", help="sets status of task to done")
+done_parser.add_argument("val", type=int, help="id of task")
+
 #Arguments
 args = parser.parse_args()
 
@@ -96,6 +104,44 @@ def del_task(num):
         json.dump(data, f, indent=4)
         print('Task successfully updated.')
 
+#Function for "mark-in-progress" parser
+def in_prog(val):
+    file = "tasks.json"
+
+    #File Check
+    if os.path.isfile(file) and os.path.getsize(file) > 0:
+        with open(file, "r") as f:
+            data = json.load(f)
+    else:
+        print("Create a task first.")
+        return None
+    
+    data["tasks"][val]["status"] = "in-progress"
+    data["tasks"][val]["updatedAt"] = neat
+
+    with open(file, "w") as f:
+        json.dump(data, f, indent=4)
+        print('Task successfully updated.')
+
+#Function for "mark-done" parser
+def mk_done(val):
+    file = "tasks.json"
+
+    #File Check
+    if os.path.isfile(file) and os.path.getsize(file) > 0:
+        with open(file, "r") as f:
+            data = json.load(f)
+    else:
+        print("Create a task first.")
+        return None
+    
+    data["tasks"][val]["status"] = "done"
+    data["tasks"][val]["updatedAt"] = neat
+
+    with open(file, "w") as f:
+        json.dump(data, f, indent=4)
+        print('Task successfully updated.')
+
 #Command List
 if args.command == "add":
     new_task(args.name)
@@ -103,5 +149,9 @@ elif args.command == "update":
     new_name(args.new_id, args.new)
 elif args.command == "delete":
     del_task(args.num)
+elif args.command == "mark-in-progress":
+    in_prog(args.val)
+elif args.command == "mark-done":
+    mk_done(args.val)
 else:
     print("Failed to execute.")
